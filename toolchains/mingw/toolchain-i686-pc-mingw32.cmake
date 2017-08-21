abacbc5f7ca663a30415c3c46ea52a1529452bfd
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2017-08-21 20:26:50 +0200

    fix mingw compilation

--- toolchains/mingw/toolchain-i686-pc-mingw32.cmake
+++ toolchains/mingw/toolchain-i686-pc-mingw32.cmake
@@ -2,12 +2,12 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER i686-pc-mingw32-gcc)
-set(CMAKE_CXX_COMPILER i686-pc-mingw32-g++)
-set(CMAKE_RC_COMPILER i686-pc-mingw32-windres)
+set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/i686-pc-mingw32)
+set(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
